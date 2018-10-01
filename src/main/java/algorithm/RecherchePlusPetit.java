fed0b17c68dd263311862710cc4774d49842da95
bankass79 <a.guindo@LGOP1378>
2018-10-01 17:12:58 +0200

    use scanner class

--- src/main/java/algorithm/RecherchePlusPetit.java
+++ src/main/java/algorithm/RecherchePlusPetit.java
@@ -1,10 +1,29 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class RecherchePlusPetit {
 
 	public  int plusPetitElement(int [] n) {
 
-
+	
+	  System.out.println(" Saisissez la taille du tableau :");		  
+	  Scanner sc = new Scanner (System.in);		
+	  
+		  // n tableau d'entier, s la taille du tableau
+		   int s= sc.nextInt();
+		 		   n = new int [s];
+		 		   
+		 		   int taille= n.length;
+	  		  
+		 		   
+		 		   for (int i = 0; i < taille; i++) {
+			  System.out.println(" saisisse : " + " " + i + " ème Valeur  du tableau");
+			  
+		   n [i] = sc.nextInt();
+		  
+	}
+		 sc.close();
 		// k est l'indice du premier élement
 		int k =0 ;
 
@@ -15,6 +34,8 @@ public class RecherchePlusPetit {
 			}
 
 		}
+		
+		System.out.println("l'indice de la plus petite valeur du tableau est: ");
 		return k;
 
 
